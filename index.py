68153d5743b1cf60a0d651e848837de8a55fa0c0
kremilly <contact@kremilly.com>
2024-04-03 09:19:56 -0300

    feat: disable cache for fix errors on request

--- index.py
+++ index.py
@@ -1,18 +1,10 @@
 import os
 
-from flask_caching import Cache
 from flask import Flask, render_template
 
 from core.github_endpoints import GitHubEndpoints
 
 app = Flask(__name__)
-app.config.from_mapping({
-    "DEBUG": True,
-    "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 300
-})
-
-cache = Cache(app)
 
 @app.route("/")
 def index():
@@ -23,7 +15,6 @@ def index():
     )
 
 @app.route('/api', methods=['GET'])
-@cache.cached(timeout=180)
 def get_pinneds():
     return GitHubEndpoints.repos_pinned()
 
